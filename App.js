fdb922d7836d272e9288d91a49af34f64ca50e41
AdrianS100304 <jose13699631@gmail.com>
2024-02-13 18:42:49 -0600

    Prueba tecnica

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+import Home from './src/pages/home/home'
+import Series from './src/pages/series/series';
+import Peliculas from './src/pages/peliculas/peliculas';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          options={{ headerShown: false }}
+          component={Home}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { Provider } from 'react-redux';
+// import store from './Redux/store/index';
+// import Home from './src/pages/home/home';
+// import FooterScreen from './src/pages/footer/footer';
+// import HeaderScreen from './src/pages/header/header';
+// const Stack = createNativeStackNavigator();
+
+// export default function App() {
+//   return (
+//     <Provider store={store}>
+//       <NavigationContainer>
+//         <Stack.Navigator>
+//           <Stack.Screen name="Home" options={{ headerShown: false }} component={Home} />
+//         </Stack.Navigator>
+//       </NavigationContainer>
+//     </Provider>
+//   );
+// }
